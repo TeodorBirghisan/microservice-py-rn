5d4cfd7feeac5de6cc43ef4ae3be34619d41506c
Teodor Birghisan <birghisanteodor@yahoo.com>
2021-12-05 18:10:11 +0200

    Defined products route

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):  # /api/products
+        pass
+
+    def get(self, request, pk=None):  # /api/products/:id
+        pass
+
+    def update(self, request, pk=None):  # /api/products/:id
+        pass
+
+    def delete(self, request, pk=None):  # /api/products/:id
+        pass
