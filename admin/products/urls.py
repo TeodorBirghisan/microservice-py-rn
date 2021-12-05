2ab292c2faa126b2d328824c3aa20632f92c95de
Teodor Birghisan <birghisanteodor@yahoo.com>
2021-12-05 18:33:07 +0200

    Endpoint to get random user

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import ProductViewSet
+from .views import ProductViewSet, UserAPIView
 
 urlpatterns = [
     path('products', ProductViewSet.as_view(
         {'get': 'list', 'post': 'create'})),
     path('products/<str:pk>',
-         ProductViewSet.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'}))
+         ProductViewSet.as_view({'get': 'get', 'put': 'update', 'delete': 'delete'})),
+    path('user', UserAPIView.as_view())
 ]
